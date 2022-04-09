54ca969dad8a34910f90c44f92cc574e6964be25
avanticoders <avanticoders@gmail.com>
2022-04-09 13:29:44 +0300

    trial

--- Ampics/urls.py
+++ Ampics/urls.py
@@ -13,9 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -23,8 +22,8 @@ from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
     path('admin/', admin.site.urls),
     path('', include('website.urls'))
