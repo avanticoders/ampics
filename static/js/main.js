9b03efc1305bcbdead4e4f8cda341aa3754d7159
avanticoders <avanticoders@gmail.com>
2023-04-30 15:21:44 +0300

    Static

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+function open_menu() {
+    let menu = document.querySelector('#menu');
+    menu.style.display = 'flex';
+}
+
+function close_menu() {
+    let menu = document.querySelector('#menu');
+    menu.style.display = 'none';
+}
+
+function open_dash_menu() {
+    let menu = document.querySelector('#dash');
+    menu.style.display = 'flex';
+}
+
+function close_dash_menu() {
+    let menu = document.querySelector('#dash');
+    menu.style.display = 'none';
+}
